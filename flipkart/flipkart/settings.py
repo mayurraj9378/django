3605d9d84c36f48b6ca64beee087fa7a2e9e67a9
Mayurraj9378 <mrajgude2003@gmail.com>
2025-03-19 16:51:44 +0530

    second commit

--- flipkart/flipkart/settings.py
+++ flipkart/flipkart/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Build paths inside the project like this: BASE_DIR / 'subdir'
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- flipkart/flipkart/urls.py
+++ flipkart/flipkart/urls.py
@@ -15,11 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include ,path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from electronics import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-    path('electronics/',include('electronics.urls')),
+    path('', views.home, name='home'),
+    path('electronics/', include('electronics.urls')),
 ]
+
+# Add this for serving media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
